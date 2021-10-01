e1423bb31b17b0cefc82d152f1f0f870d2223ba9
Yoongyeong Song <14dnfnfn@gmail.com>
2021-10-02 01:26:12 +0900

    #11 [feature/main] 메인 페이지 XML 제작 - colors.xml에 shadow 컬러 삽입 - strings.xml에 에러 메시지 및 description 메시지 삽입 - img bus tag 삽입 (컬러 값 수정) - 데이터 바인딩 적용하여 xml 작성

--- app/src/main/java/com/pns/bbasdriver/MainActivity.kt
+++ app/src/main/java/com/pns/bbasdriver/MainActivity.kt
@@ -2,10 +2,12 @@ package com.pns.bbasdriver
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.pns.bbasdriver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
