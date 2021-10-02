45ab240d1d092de3910b8dc2894e79b7f9e7d530
Yoongyeong Song <14dnfnfn@gmail.com>
2021-10-02 18:26:41 +0900

    #15 [feature/driving] 주행중 페이지 생성 및 가로모드 고정 설정

---
+++ app/src/main/java/com/pns/bbasdriver/DrivingActivity.kt
@@ -0,0 +1,11 @@
+package com.pns.bbasdriver
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DrivingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_driving)
+    }
+}
