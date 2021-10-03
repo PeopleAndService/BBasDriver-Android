cd370ad9c982698444c5c11bee31ab40ede1ef44
Yoongyeong Song <14dnfnfn@gmail.com>
2021-10-04 00:52:56 +0900

    #19 [feature/driving] 주행중 다이얼로그 생성 함수 생성

--- app/src/main/java/com/pns/bbasdriver/DrivingActivity.kt
+++ app/src/main/java/com/pns/bbasdriver/DrivingActivity.kt
@@ -1,11 +1,66 @@
 package com.pns.bbasdriver
 
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.pns.bbasdriver.databinding.ActivityDrivingBinding
 
 class DrivingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDrivingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_driving)
+        binding = ActivityDrivingBinding.inflate(layoutInflater)
+        binding.ivBus.setOnClickListener {
+            createRindingDialog()
+        }
+        binding.groupTxtBusStop.setHeight(
+            TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_SP,
+                24 * 4f,
+                this.resources.displayMetrics
+            ).toInt()
+        )
+        setContentView(binding.root)
+    }
+
+    private fun View.setHeight(value: Int) {
+        val lp = layoutParams
+        lp?.let {
+            lp.height = value
+            layoutParams = lp
+        }
+    }
+
+    private fun createWaitDialog() {
+        MaterialAlertDialogBuilder(this)
+            .setTitle(resources.getString(R.string.wait_title))
+            .setMessage(resources.getString(R.string.wait_msg))
+            .setPositiveButton(resources.getString(R.string.btn_confirm)) { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
+    }
+
+    private fun createRindingDialog() {
+        binding.isRiding = true
+        MaterialAlertDialogBuilder(this)
+            .setTitle(resources.getString(R.string.riding_tile))
+            .setMessage(resources.getString(R.string.riding_msg))
+            .setPositiveButton(resources.getString(R.string.btn_confirm)) { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
+    }
+
+    private fun createEndDialog(busNumber: Int) {
+        MaterialAlertDialogBuilder(this)
+            .setTitle(resources.getString(R.string.driving_finish_title))
+            .setMessage(resources.getString(R.string.driving_finish_msg, busNumber))
+            .setPositiveButton(resources.getString(R.string.btn_confirm)) { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
     }
 }
