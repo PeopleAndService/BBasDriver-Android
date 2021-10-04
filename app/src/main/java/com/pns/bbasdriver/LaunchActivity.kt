866ca6a5b9ed14b101232ef10cd351c1e82708b2
Yoongyeong Song <14dnfnfn@gmail.com>
2021-10-05 02:19:20 +0900

    #31 [feature/onboard] splash Activity 생성

---
+++ app/src/main/java/com/pns/bbasdriver/LaunchActivity.kt
@@ -0,0 +1,13 @@
+package com.pns.bbasdriver
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LaunchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
